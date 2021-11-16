b24ac179bac763c4c07e06aede093a73561f58b6
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-11-16 10:21:28 -0400

    V0.1 ( does not work ) need to keep trying...

--- InterativeFiction/Program.cs
+++ InterativeFiction/Program.cs
@@ -8,8 +8,80 @@ namespace InterativeFiction
 {
     class Program
     {
+        static bool gameOver = false;
+        static int x = 0;
+        static int y = 0;
+        static string[,] story = new string[,]
+        {
+
+        {"Title","Page1"},
+        {"Page2","Page3"},
+        {"Your on page 2","your on page 3" },
+        {"Go to page2","Go to page3",},
+
+        };
+
+
+
         static void Main(string[] args)
         {
+              // how am I supposed to do this......
+             // GameLoops for player interaction
+            // I need to make triggers - when button is pressed choice is displayed (if statments)
+           // Use string array for story text
+
+            while (gameOver == false)
+            {
+                Console.WriteLine(story[1, 1]);
+                Console.WriteLine("");
+                Console.WriteLine(story[1, 2]);
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("<A> "+ story[4, 1]);
+                Console.WriteLine("<B> " + story[4, 2]);
+
+
+            }
+
+
         }
+        static void Inputs()
+        {
+
+            ConsoleKeyInfo Input = Console.ReadKey(intercept: true);
+
+            if (Input.Key == ConsoleKey.A)
+            {
+                Console.Clear();
+                Console.Write(story[2, 1]);
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine(story[3,1]);
+
+            }
+            else if (Input.Key == ConsoleKey.B)
+            {
+                Console.Clear();
+                Console.Write(story[2, 2]);
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine(story[3, 2]);
+            }
+            else if (Input.Key == ConsoleKey.Escape)
+            {
+                Console.Clear();
+                Console.WriteLine(story[1, 1]);
+                Console.WriteLine("");
+                Console.WriteLine(story[1, 2]);
+
+            }
+
+        }
+
+
+
+
+
     }
 }
